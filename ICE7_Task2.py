56f27eb7f7728e62c2b47611939c99f4b56a7fae
mahmood247 <34489725+mahmood247@users.noreply.github.com>
2017-12-12 15:16:44 -0600

    Create ICE7_Task2.py

---
+++ ICE7_Task2.py
@@ -0,0 +1,24 @@
+import random
+import gym
+import numpy as np
+
+EPISODES = 900
+
+if __name__ == "__main__":
+    env = gym.make('MountainCar-v0')
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    done = False
+    batch_size = 30
+
+    for e in range(EPISODES):
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+        for time in range(500):
+            env.render()
+            next_state, reward, done, _ = env.step(random.randrange(3))
+            reward = reward if not done else -10
+            next_state = np.reshape(next_state, [1, state_size])
+            state = next_state
+            if done:
+                break
